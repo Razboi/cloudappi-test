6afef4da358e1de1448001709085c03712c69a2d
Marc <recatala.marc@gmail.com>
2019-11-04 14:58:57 +0100

    Removed useless models imports on controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,3 @@
-const User = require("../models/User");
-const Address = require("../models/Address");
 const errors = require("../utils/errors");
 const usersService = require("../services/users");
 
